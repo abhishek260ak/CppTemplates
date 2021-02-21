20ae1f8d3a279384ce426483b3c0c4cc04d1ccce
Abhishek Kumar <abhishekkumar260ak@gmail.com>
2021-02-21 12:09:34 +0530

    First commit
    Sum of n natural nos. recursively

---
+++ n_natural_recursive/n_natural_recursive/n_natural_recursive.cpp
@@ -0,0 +1,24 @@
+//Sum of 'n' natural numbers - Recusive Solution
+//Abhishek Kumar - @abhishek260ak
+
+#include<iostream>
+using namespace std;
+
+int sum(int n)
+{
+	if (n > 0)
+	{
+		return { sum(n - 1) + n };
+	}
+
+	else return n;
+};
+
+int main()
+{
+	int n{ 0 };
+	cout << "Enter the integer upto which sum is  required" << endl;
+	cin >> n;
+	cout << "The sum is " << sum(n) << endl;
+	return 0;
+}
